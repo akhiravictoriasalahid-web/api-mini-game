3a3afc5965e530a7a2c27643f656fa7c15b14fb7
akhiravictoriasalahid-web <akhiravictoriasalahid@gmail.com>
2026-05-07 10:27:07 +0200

    Add login validation and form state handling

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,16 +4,48 @@ import "./Login.css";
 
 
 function Login() {
+
+    const navigate = useNavigate();
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+
+        const savedUsername = localStorage.getItem("username");
+        const savedPassword = localStorage.getItem("password");
+
+        if (
+            username === savedUsername &&
+            password === savedPassword
+        ) {
+            navigate("/game");
+        } else {
+            alert("Invalid username or password");
+        }
+    };
+    
     return (
         <div className="card">
             <h2>Welcome to My Mini Game</h2>
             <p>Login to Continue</p>
 
             <div className="form">
-                <input type="text" id="username" placeholder="Username..." />
-                <input type="password" id="password" placeholder="Password" />
+                <input 
+                type="text" 
+                placeholder="Username..." 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                />
 
-                <button>LOgin</button>
+                <input 
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button onClick={handleLogin}>Login</button>
 
                 <p className="signup-text">
                     Don't have an account?
